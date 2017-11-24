628f70c35eafd6d74ec665a699210077810cb734
ProLoD <r0668988@kuleuven.be>
2017-11-24 23:00:33 +0200

    partial implementation function fro nearest users

--- junction2017/polls/views.py
+++ junction2017/polls/views.py
@@ -1,5 +1,27 @@
 from django.http import HttpResponse
+from polls.models import User
+# pip install gpxpy --user
+import gpxpy.geo
 
-def index(request):
+def get_nearest_users(current_user_id):
+    current_user = User.objects.filter(current_user_id=user_id).first()
+    current_user_lon = current_user.location_lon
+    current_user_lat = current_user.location_lon
+    users = User.objects.all()
+    nearest_users = list()
     output = ""
-    return HttpResponse(output)
+    for u in users:
+        if len(nearest_users) <= 5:
+            nearest_users.append(u)
+        else:
+            u_lon = u.location_lon
+            u_lan = u.location_lat
+            nearest_users.sort(key=lambda x: gpxpi.geo.haversine_distance(u_lat, u_lon, , current_user_lat))
+
+    return nearest_users
+
+
+
+    # #PROCESS THE POST REQUEST @TOBIAS
+    # @api_view(['POST'])
+    # def post_location(request, format=None):
