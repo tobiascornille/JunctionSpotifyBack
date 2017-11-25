ee5f610c288a6683a723723f90ba5c36353267e4
Tobias Cornille <cornilletobias@gmail.com>
2017-11-25 22:06:10 +0200

    Debug

--- junction2017/users/urls.py
+++ junction2017/users/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^([\w.@+-]+)&([\w.@+-]+)&([\w.@+-]+)', views.user_data, name='user_data'),
-    url(r'^$', views.create_user),
+    url(r'^users/([\w.@+-]+)&([\w.@+-]+)&([\w.@+-]+/$)', views.user_data, name='user_data'),
+    url(r'^users/$', views.create_user),
     # url(r'^postrequest', views.GET),
     # url(r'^spotipy', views.get_tracks)
 ]
