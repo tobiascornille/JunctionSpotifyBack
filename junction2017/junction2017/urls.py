99e552e17a7dde68c75a701bcc79114e2acf48db
Tobias Cornille <cornilletobias@gmail.com>
2017-11-25 19:40:58 +0200

    Started working on the playlist endpoint

--- junction2017/junction2017/urls.py
+++ junction2017/junction2017/urls.py
@@ -18,5 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^users/', include('users.urls')),
+    url(r'^playlist/', include('playlist.urls'))
     url(r'^admin/', admin.site.urls),
 ]
---
+++ junction2017/playlist/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+    #url(r'^postrequest', views.GET),
+    url(r'^spotipy', views.get_tracks)
+]
