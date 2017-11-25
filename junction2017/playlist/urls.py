15325ba6ce8fd3dae3da4376344d7ca00bd614f0
Tobias Cornille <cornilletobias@gmail.com>
2017-11-25 20:59:17 +0200

    Implemented the playlist endpoint

--- junction2017/playlist/urls.py
+++ junction2017/playlist/urls.py
@@ -2,7 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    #url(r'^postrequest', views.GET),
-    url(r'^spotipy', views.get_tracks)
+        url(r'^playlist/$', views.create_playlist(), name='playlist'),
+        url(r'^playlist/(?P<user_id>\S+)&(?P<track_id>\S+)', views.modify_track(), name='playlist-track'),
 ]
--- junction2017/playlist/views.py
+++ junction2017/playlist/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from users.models import User
+import spotipy
+import rest_framework
 
-# Create your views here.
+@api_view(['POST'])
+def create_playlist(request, user_id):
+    # Create a new playlist
+    user = User.objects.filter(user_id=user_id).first()
+    token = user.token
+    if token:
+        spotify = spotipy.Spotify(auth=token)
+        playlist = spotify.user_playlist_create(user_id, "Cirkel")
+        user.playlist_id = playlist["id"]
+        user.save
+
+
+@api_view(['POST', 'DELETE'])
+def modify_track(request, user_id, track_id):
+    # Add or delete a track to the user's playlist
+    user = User.objects.filter(user_id=user_id).first()
+    token = user.token
+    playlist_id = user.playlist
+
+    if token:
+        spotify = spotipy.Spotify(auth=token)
+        results="Somethings wrong :/"
+
+        if request.method == 'POST':
+            results = spotify.user_playlist_add_tracks(user_id, playlist_id, list(track_id))
+        elif request.method == 'DELETE':
+            results = user_playlist_remove_all_occurrences_of_tracks(user_id, playlist_id, list(track_id))
