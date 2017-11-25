ac84cc5f0011ae2bf2be91e2a606147e36bcaf34
ProLoD <r0668988@kuleuven.be>
2017-11-25 12:58:21 +0200

    authentication works

--- junction2017/users/views.py
+++ junction2017/users/views.py
@@ -9,41 +9,36 @@ import spotipy.util as util
 
 def get_nearest_users(current_user_id):
     current_user = User.objects.filter(user_id=current_user_id).first()
-    print(current_user)
     current_user_lon = current_user.location_lon
     current_user_lat = current_user.location_lat
     users = User.objects.all()
     nearest_users = []
-    for i in range(0,5):
-        closest = None
-        for u in users:
-            if u != current_user and u not in nearest_users:
-                if closest == None:
-                    closest = u
-                else:
-                    closest = nearest_user(current_user, closest, u)
-        nearest_users.append(closest)
+    if len(users) < 6:
+        limit_users = len(users) - 1
+    else:
+        limit_users = 5
+    for i in range(0, limit_users):
+        min_distance = math.inf
+        closest_user = None
+        for user in users:
+            if user != current_user and user not in nearest_users:
+                distance = distance_between(current_user, user)
+                if distance < min_distance:
+                    min_distance = distance
+                    closest_user = user
+        nearest_users.append(closest_user)
 
     return nearest_users
 
 # helper function
-def nearest_user(current, u1, u2):
+def distance_between(u1, u2):
     u1_lon = u1.location_lon
     u1_lat = u1.location_lat
 
     u2_lon = u2.location_lon
     u2_lat = u2.location_lat
 
-    c_lon = current.location_lon
-    c_lat = current.location_lat
-
-    dist_u1 = gpxpy.geo.haversine_distance(u1_lat, u1_lon, c_lat, c_lon)
-    dist_u2 = gpxpy.geo.haversine_distance(u2_lat, u2_lon, c_lat, c_lon)
-
-    if (dist_u1 < dist_u2):
-        return u1
-    else:
-        return u2
+    return gpxpy.geo.haversine_distance(u1_lat, u1_lon, u2_lat, u2_lon)
 
 def index(request):
     output = ""
@@ -63,10 +58,10 @@ def show_tracks(request):
     return output
 
 def get_tracks(request):
-    scope = "user-library-read"
-    c_id =  'f6e830e710d4157b2b47a6a76fb7cf5'
+    scope = "user-library-read user-read-private user-read-email user-read-birthdate"
+    c_id =  '7f6e830e710d4157b2b47a6a76fb7cf5'
     c_secret = '65ab11b0ad8c43feb801be2675dc175c'
-    uri = 'http://localhost:8000/users/spotipy/'
+    uri = 'https://localhost:8888/callback'
     if len(sys.argv) > 1:
         username = sys.argv[1]
     else:
@@ -76,10 +71,10 @@ def get_tracks(request):
 
     token = util.prompt_for_user_token(username, scope, client_id=c_id, client_secret=c_secret,redirect_uri=uri)
     output = ""
-
     if token:
         sp = spotipy.Spotify(auth=token)
         playlists = sp.user_playlists(username)
+        output += '<h1>' + str(sp.me()['display_name']) + '</h1><br>'
         for playlist in playlists['items']:
             if playlist['owner']['id'] == username:
                 results = sp.user_playlist(username, playlist['id'],
@@ -97,7 +92,3 @@ def POST(request):
     data = json.load(request)
     print(JsonRepsponse(data=data))
     return JsonRepsponse(data=data)
-
-    # #PROCESS THE POST REQUEST @TOBIAS
-    # @api_view(['POST'])
-    # def post_location(request, format=None):
