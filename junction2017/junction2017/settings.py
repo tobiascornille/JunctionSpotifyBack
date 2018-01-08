1247d3adfad12af20a22284d21dc1f16aea9e645
ProLoD <r0668988@kuleuven.be>
2018-01-08 11:33:14 +0100

    django.corsheaders deleted: no functionality and caused bug (SECRET_KEY not found)

--- junction2017/junction2017/settings.py
+++ junction2017/junction2017/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-from corsheaders.defaults import default_headers
+# from corsheaders.defaults import default_headers
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -140,7 +140,7 @@ CORS_ALLOW_METHODS = (
     'POST',
 )
 
-CORS_ALLOW_HEADERS = default_headers
+# CORS_ALLOW_HEADERS = default_headers
 
 # CSRF_TRUSTED_ORIGINS = (
 #     'localhost/cirkel',
