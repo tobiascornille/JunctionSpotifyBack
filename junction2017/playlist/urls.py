94225a0850f891a322504a08b045b080a272e5df
ProLoD <r0668988@kuleuven.be>
2018-01-11 20:30:22 +0100

    made structure more logical and compatible with heroku

--- junction2017/playlist/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import url
-from . import views
-
-urlpatterns = [
-        url(r'^$', views.create_playlist, name='playlist'),
-        url(r'^track$', views.modify_track, name='playlist-track'),
-]
