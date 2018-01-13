38c921720e009d6d47605b5009d36952a389afbf
ProLoD <r0668988@kuleuven.be>
2018-01-13 13:07:19 +0100

    still problem with database

--- JunctionSpotifyBack/settings.py
+++ JunctionSpotifyBack/settings.py
@@ -90,8 +90,7 @@ WSGI_APPLICATION = 'JunctionSpotifyBack.wsgi.application'
 #     }
 # }
 
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+DATABASES = { 'default': dj_database_url.config() }
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
